e4b5585cc693aabfeabc176d706358ba2f003cbd
Obeng88 <joseph.mensah@talenti.jobsacademy.org>
2026-04-08 12:06:53 +0200

    cambiato gli ip

--- main_class.py
+++ main_class.py
@@ -14,7 +14,7 @@ app=FastAPI()
 '''Configurazione CORS per permettere al frontend di comunicare con il backend'''
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["127.0.0.1:7001"],  # in produzione metti il tuo dominio
+    allow_origins=["*"],  # in produzione metti il tuo dominio
     allow_methods=["*"],
     allow_headers=["*"],
 )
